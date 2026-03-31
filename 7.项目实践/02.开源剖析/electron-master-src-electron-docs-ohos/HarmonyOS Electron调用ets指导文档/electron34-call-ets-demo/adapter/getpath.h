2b847d19f8b5e687e0ccab11c8a6ffe69f9e3953
易文强 <303575398@qq.com>
2026-03-31 17:54:01 +0800

    feat(docs): delete  harmony electron-ohos

--- 7.项目实践/02.开源剖析/electron-master-src-electron-docs-ohos/HarmonyOS Electron调用ets指导文档/electron34-call-ets-demo/adapter/getpath.h
+++
@@ -1,6 +0,0 @@
-#include <string>
-
-namespace PathAdapter {
-std::string __attribute__((visibility("default"))) GetDir(std::string& getDirFuncName);
-
-}
--- 7.项目实践/02.开源剖析/electron-master-src-electron-docs-ohos/HarmonyOS Electron调用ets指导文档/electron34-call-ets-demo/addon/hello.js
+++
@@ -1,3 +0,0 @@
-const addon = require('bindings')('electron-addon');
-
-console.log(addon.hello()); // 'world'
